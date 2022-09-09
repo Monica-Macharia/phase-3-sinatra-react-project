d65229e04806b16924f6b1dd5e582379e6d3ce4e
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-09-09 11:18:24 +0300

    webserver

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,19 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good ur project!" }.to_json
+  get '/employees' do
+    "Hello world"
+   
   end
+ 
+  post '/employees' do
+    employees = Employee.create(name:params[:name],
+    location:params[:location], contact:params[:contact], comment:params[:comment]) 
+    employees.to_json
+  end
+ 
+ 
+
+
 
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,5 @@
 class Employee < ActiveRecord::Base
     has_many :tasks
     has_many :customers, through: :tasks
+    
 end
