a1ff8bd263c624cf68de4e2adf24244d65f1274b
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-09-09 12:05:56 +0300

    CRUD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,67 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get '/employees' do
-    "Hello world"
+    employees = Employee.all
+    employees.to_json
    
   end
+  
+  get '/customers' do
+    customers = Customer.all
+    customers.to_json
+  end
+
+  get '/tasks' do
+    tasks = Task.all
+    tasks.to_json
+  end
+
  
   post '/employees' do
     employees = Employee.create(name:params[:name],
     location:params[:location], contact:params[:contact], comment:params[:comment]) 
     employees.to_json
   end
+
+  post '/customers' do
+    customers = Customer.create(name:params[:name],
+    location:params[:location], contact:params[:contact], comment:params[:comment]) 
+    customers.to_json
+  end
+
+  post '/tasks' do
+    tasks = Task.create(name:params[:name],
+    price:params[:price], status:params[:status], customer_id:params[:customer_id], employee_id:params[:employee_id]) 
+    tasks.to_json
+  end
+  
+  delete '/employees/:id' do
+    employee = Employee.find(params[:id])
+    employee.destroy
+    employee.to_json
+  end
+
+  delete '/customers/:id' do
+    customer = Customer.find(params[:id])
+    customer.destroy
+    customer.to_json
+  end
+
+  delete '/tasks/:id' do
+    task = Task.find(params[:id])
+    task.destroy
+    task.to_json
+  end
+
+  put '/customers/:id' do
+    customer = Customer.find(params[:id])
+    customer.update(
+      price: params[:price]
+    )
+    customer.to_json
+  end
  
+  
  
 
 
